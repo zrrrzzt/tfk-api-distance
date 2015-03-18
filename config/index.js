9a18bdbb5646ac2130f22e6c7d3b1191f6e40b2e
Geir Gåsodden <geir.gasodden@pythonia.no>
2015-03-18 17:25:27 +0100

    Initial commit

--- config/index.js
+++ config/index.js
@@ -2,7 +2,8 @@
 
 var config = {
   'SERVER_PORT':3000,
-  'DB': 'mongodb://localhost:27017/tfk'
+  'DB': 'mongodb://localhost:27017/tfk',
+  'API_KEY': 'AIzaSyAu4oe2bKCeP4AnAyo78KL_XZvrS-WVIcw'
 };
 
 module.exports = config;
--- handlers/index.js
+++ handlers/index.js
@@ -2,7 +2,8 @@
 
 var distance = require('google-distance');
 var xmlify = require('xmlify');
-var startEmbedUrl= 'https://www.google.com/maps/embed/v1/directions?key=AIzaSyAu4oe2bKCeP4AnAyo78KL_XZvrS-WVIcw&mode=walking&origin=';
+var config = require('../config');
+var startEmbedUrl= 'https://www.google.com/maps/embed/v1/directions?key='+config.API_KEY+'&mode=walking&origin=';
 
 function getDistance(request, reply) {
 
@@ -23,7 +24,7 @@ function getDistance(request, reply) {
           reply(err);
         }
       } else {
-        date.directionsEmbedUrl = embedUrl;
+        data.directionsEmbedUrl = embedUrl;
         if (request.query.format === 'xml') {
           reply(xmlify(data)).type('application/xml');
         } else {
